e1ac746a4e85a9981fbfa1bd71d415061b424331
Markus Bach <git@dingensundso.de>
2024-07-09 18:20:43 +0200

    move game into class

--- src/main.ts
+++ src/main.ts
@@ -9,124 +9,128 @@ if (window.DEBUG) {
 
 const games_div = document.getElementById("games");
 const scoreDisplay = document.getElementById("score");
-const width = 10;
 
-let currentSnake = [2, 1, 0];
-let direction = 0;
-let nextDirection = 1;
-let score = 0;
-const intervalTime = 800;
-let interval = 0;
-
-const grid = document.createElement("div");
-grid.classList.add("grid");
-games_div.appendChild(grid);
-
-let squares = grid.querySelectorAll("div");
-
-function resetGrid() {
-	grid.textContent = "";
-	for (let i = 0; i < 100; i++) {
-		grid.appendChild(document.createElement("div"));
-	}
-	squares = grid.querySelectorAll("div");
-}
-
-function startGame() {
-	nextDirection = 1;
+class SnakeGame {
+	width = 10;
 	currentSnake = [2, 1, 0];
-	for (const idx of currentSnake) {
-		squares[idx].classList.add("snake");
-	}
-	randomApple();
-	interval = setInterval(stepGame, intervalTime);
-}
+	direction = 0;
+	nextDirection = 1;
+	score = 0;
+	intervalTime = 800;
+	interval = 0;
+	dom: HTMLElement;
+	squares: NodeListOf<HTMLElement>;
 
-function stepGame() {
-	if (nextDirection !== -direction) direction = nextDirection;
-	if (collision()) {
-		gameOver();
-	} else {
-		moveSnake();
-	}
-}
+	constructor(width = 10) {
+		this.width = width;
+		this.dom = document.createElement("div");
+		this.dom.classList.add("grid");
 
-function moveSnake() {
-	const tail = currentSnake.pop();
-	squares[tail].classList.remove("snake");
-	currentSnake.unshift(currentSnake[0] + direction);
-	const head = squares[currentSnake[0]];
-	head.classList.add("snake");
-	// check apple
-	if (head.classList.contains("apple")) {
-		head.classList.remove("apple");
-		score += 1;
-		grid.dispatchEvent(
-			new CustomEvent("scoreUpdate", { detail: score, bubbles: true }),
+		for (let i = 0; i < 100; i++) {
+			this.dom.appendChild(document.createElement("div"));
+		}
+		this.squares = this.dom.querySelectorAll("div");
+		this.startGame();
+	}
+	startGame() {
+		document.addEventListener("keydown", (event) => {
+			if (event.defaultPrevented) {
+				return;
+			}
+
+			switch (event.key) {
+				case "ArrowRight":
+					this.nextDirection = 1;
+					break;
+				case "ArrowLeft":
+					this.nextDirection = -1;
+					break;
+				case "ArrowDown":
+					this.nextDirection = this.width;
+					break;
+				case "ArrowUp":
+					this.nextDirection = -this.width;
+					break;
+			}
+		});
+		for (const idx of this.currentSnake) {
+			this.squares[idx].classList.add("snake");
+		}
+		this.randomApple();
+		this.interval = setInterval(this.stepGame.bind(this), this.intervalTime);
+	}
+
+	stepGame() {
+		if (this.nextDirection !== -this.direction)
+			this.direction = this.nextDirection;
+		if (this.collision) {
+			this.gameOver();
+		} else {
+			this.moveSnake();
+		}
+	}
+
+	moveSnake() {
+		const tail = this.currentSnake.pop();
+		this.squares[tail].classList.remove("snake");
+		this.currentSnake.unshift(this.currentSnake[0] + this.direction);
+		const head = this.squares[this.currentSnake[0]];
+		head.classList.add("snake");
+		// check apple
+		if (head.classList.contains("apple")) {
+			head.classList.remove("apple");
+			this.score += 1;
+			this.dom.dispatchEvent(
+				new CustomEvent("scoreUpdate", { detail: this.score, bubbles: true }),
+			);
+			this.currentSnake.push(tail);
+			this.squares[tail].classList.add("snake");
+			this.randomApple();
+		}
+	}
+
+	get collision() {
+		if (
+			(this.currentSnake[0] + this.width >= this.width * this.width &&
+				this.direction === this.width) ||
+			(this.currentSnake[0] % this.width === this.width - 1 &&
+				this.direction === 1) ||
+			(this.currentSnake[0] % this.width === 0 && this.direction === -1) ||
+			(this.currentSnake[0] - this.width <= 0 &&
+				this.direction === -this.width) ||
+			this.squares[this.currentSnake[0] + this.direction].classList.contains(
+				"snake",
+			)
+		) {
+			return true;
+		}
+		return false;
+	}
+
+	randomApple() {
+		const availableSquares = this.dom.querySelectorAll(
+			'div[class=""], div:not([class])',
 		);
-		currentSnake.push(tail);
-		squares[tail].classList.add("snake");
-		randomApple();
-	}
-}
+		const idx = Math.floor(Math.random() * availableSquares.length);
 
-function collision() {
-	if (
-		(currentSnake[0] + width >= width * width && direction === width) ||
-		(currentSnake[0] % width === width - 1 && direction === 1) ||
-		(currentSnake[0] % width === 0 && direction === -1) ||
-		(currentSnake[0] - width <= 0 && direction === -width) ||
-		squares[currentSnake[0] + direction].classList.contains("snake")
-	) {
-		return true;
-	}
-	return false;
-}
-
-function randomApple() {
-	const availableSquares = grid.querySelectorAll(
-		'div[class=""], div:not([class])',
-	);
-	const idx = Math.floor(Math.random() * availableSquares.length);
-
-	availableSquares[idx].classList.add("apple");
-}
-
-function gameOver() {
-	clearInterval(interval);
-	const popup = document.createElement("div");
-	popup.classList.add("popup");
-	const popupText = document.createElement("span");
-	popupText.innerText = "GAME OVER";
-	popup.appendChild(popupText);
-	grid.insertBefore(popup, grid.children[0]);
-	grid.dispatchEvent(new CustomEvent("gameOver", { bubbles: true }));
-}
-
-document.addEventListener("keydown", (event) => {
-	if (event.defaultPrevented) {
-		return;
+		availableSquares[idx].classList.add("apple");
 	}
 
-	switch (event.key) {
-		case "ArrowRight":
-			nextDirection = 1;
-			break;
-		case "ArrowLeft":
-			nextDirection = -1;
-			break;
-		case "ArrowDown":
-			nextDirection = width;
-			break;
-		case "ArrowUp":
-			nextDirection = -width;
-			break;
+	gameOver() {
+		clearInterval(this.interval);
+		const popup = document.createElement("div");
+		popup.classList.add("popup");
+		const popupText = document.createElement("span");
+		popupText.innerText = "GAME OVER";
+		popup.appendChild(popupText);
+		this.dom.insertBefore(popup, this.dom.children[0]);
+		this.dom.dispatchEvent(new CustomEvent("gameOver", { bubbles: true }));
 	}
-});
+}
 
 document.addEventListener("scoreUpdate", (event: CustomEvent) => {
 	scoreDisplay.innerText = event.detail;
 });
 
-resetGrid();
-startGame();
+const game = new SnakeGame();
+games_div.appendChild(game.dom);
